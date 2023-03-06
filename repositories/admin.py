da1e30143e4e061ebe21352f4d71c4cb78ddc911
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-03-06 06:00:54 +0100

    added repository functions for finding admins in the admin repository

---
+++ repositories/admin.py
@@ -0,0 +1,14 @@
+from models.admin import Admin
+from sqlalchemy.orm import Session
+
+
+def find_admin_by_id(db: Session, admin_id: int):
+    return db.query(Admin).filter(Admin.id == admin_id).first()
+
+
+def find_admin_by_email(db: Session, email: str):
+    return db.query(Admin).filter(Admin.number_plate == email).first()
+
+
+def find_admins(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Admin).offset(skip).limit(limit).all()
